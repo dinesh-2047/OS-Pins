af875896f89c0c428da046a5208f9385ffc0c99d
Md Ashad <asadanwarr0@gmail.com>
2026-02-22 15:04:03 +0530

    added the page for Cookie Policy

--- app/page.js
+++ app/page.js
@@ -555,7 +555,7 @@ export default function Home() {
                 {[
                   { name: 'Privacy Policy', href: '/privacy-policy' },
                   { name: 'Terms of Service', href: '#' },
-                  { name: 'Cookie Policy', href: '#' },
+                  { name: 'Cookie Policy', href: 'cookie-policy' },
                   { name: 'Code of Conduct', href: '#' },
                   { name: 'Contact', href: '/contact' }
                 ].map((item) => (
